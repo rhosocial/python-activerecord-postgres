b29d15812094f5199408b51a72d63581d755d342
zhao vistart <vistart@rho.social>
2026-06-15 01:20:20 +0800

    feat(relation): add bridge files for testsuite relation tests
    - Add bridge files: test_descriptor_compat, test_descriptors_async, test_validator

---
+++ tests/rhosocial/activerecord_postgres_test/feature/relation/test_descriptor_compat.py
@@ -0,0 +1,3 @@
+# tests/rhosocial/activerecord_postgres_test/feature/relation/test_descriptor_compat.py
+
+from rhosocial.activerecord.testsuite.feature.relation.test_descriptor_compat import *  # noqa: F401,F403
---
+++ tests/rhosocial/activerecord_postgres_test/feature/relation/test_descriptors_async.py
@@ -0,0 +1,3 @@
+# tests/rhosocial/activerecord_postgres_test/feature/relation/test_descriptors_async.py
+
+from rhosocial.activerecord.testsuite.feature.relation.test_descriptors_async import *  # noqa: F401,F403
