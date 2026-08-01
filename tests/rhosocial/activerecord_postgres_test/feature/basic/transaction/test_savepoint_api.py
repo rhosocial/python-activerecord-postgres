f14064eb5be594e286d9df9deb4c15083069dc5a
zhao vistart <vistart@rho.social>
2026-08-01 19:37:50 +0800

    fix(postgres): remove sync savepoint test import, only async tests apply

--- tests/rhosocial/activerecord_postgres_test/feature/basic/transaction/test_savepoint_api.py
+++ tests/rhosocial/activerecord_postgres_test/feature/basic/transaction/test_savepoint_api.py
@@ -3,5 +3,4 @@
 Black-box contracts for savepoint APIs (PostgreSQL backend).
 """
 
-from rhosocial.activerecord.testsuite.feature.basic.transaction.test_savepoint_api import *  # noqa: F403
 from rhosocial.activerecord.testsuite.feature.basic.transaction.test_savepoint_api_async import *  # noqa: F403
